e5d605395681947ea9c6be54be6027d76e322d7a
hendisantika <hendisantika@yahoo.co.id>
2024-12-01 19:31:59 +0700

    Add CompanyRequest🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/kotlin/id/my/hendisantika/reactivemongodbcrud/request/CompanyRequest.kt
@@ -0,0 +1,16 @@
+package id.my.hendisantika.reactivemongodbcrud.request
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : reactive-mongodb-crud
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 01/12/24
+ * Time: 19.31
+ * To change this template use File | Settings | File Templates.
+ */
+class CompanyRequest(
+    val name: String,
+    val address: String
+)
