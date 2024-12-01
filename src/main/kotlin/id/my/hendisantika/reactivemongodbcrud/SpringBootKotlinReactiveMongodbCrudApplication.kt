8cfd6911a2c672945ef0f052d9ecd96c85d26fcc
hendisantika <hendisantika@yahoo.co.id>
2024-12-01 19:24:32 +0700

    Create Spring Boot App🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/kotlin/id/my/hendisantika/reactivemongodbcrud/SpringBootKotlinReactiveMongodbCrudApplication.kt
@@ -0,0 +1,11 @@
+package id.my.hendisantika.reactivemongodbcrud
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootKotlinReactiveMongodbCrudApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootKotlinReactiveMongodbCrudApplication>(*args)
+}
---
+++ src/test/kotlin/id/my/hendisantika/reactivemongodbcrud/SpringBootKotlinReactiveMongodbCrudApplicationTests.kt
@@ -0,0 +1,13 @@
+package id.my.hendisantika.reactivemongodbcrud
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootKotlinReactiveMongodbCrudApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
